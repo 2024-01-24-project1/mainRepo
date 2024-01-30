73e5a5987d31bb1a7c213846448ba0ed82a401d1
kijun0708 <rlwns98@gmail.com>
2024-01-30 17:49:47 +0900

    no message

---
+++ SeoulMetro/src/com/java/common/Data.java
@@ -0,0 +1,11 @@
+package com.java.common;
+
+import java.util.ArrayList;
+
+import com.java.member.employee.EmployeeManagement;
+
+public class Data {
+	
+	public static ArrayList<EmployeeManagement> employeeManagement = new ArrayList<>();
+
+}

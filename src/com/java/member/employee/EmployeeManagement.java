e37888312f4aa7df73a85cd1c0de9345a4c41b55
kijun0708 <rlwns98@gmail.com>
2024-01-31 23:08:21 +0900

    no message

--- src/com/java/member/employee/EmployeeManagement.java
+++ src/com/java/member/employee/EmployeeManagement.java
@@ -3,19 +3,34 @@ package com.java.member.employee;
 import java.util.ArrayList;
 
 import com.java.common.Data;
-
+/**
+ * 관리자의 정보를 저장하는 클래스
+ */
 public class EmployeeManagement {
 	
-	private String id;            
-	private String pw;
-	private String name;
-	private String idNum;
-	private String phone;
-	private String rank;
-	private String line;
-	private String station;
-	private String level;
+	private String id;       		//관리자 ID     
+	private String pw;				//관리자 PW
+	private String name;			//관리자 이름
+	private String idNum;  			//관리자 주민등록번호
+	private String phone;   		//관리자 핸드폰 번호
+	private String rank;			//관리자 직급
+	private String line;			//관리자 호선
+	private String station;			//관리자 근무지
+	private String level;			//관리자 접근권한
 		
+	
+	/**
+	 * 관리자 생성자
+	 * @param id 아이디
+	 * @param pw 패스워드
+	 * @param name 이름
+	 * @param idNum 주민등록번호
+	 * @param phone 휴대폰 번호
+	 * @param rank 직급
+	 * @param line 근무지(호선)
+	 * @param station 근무지(역)
+	 * @param level 접근권한
+	 */
 	public EmployeeManagement(String id, String pw, String name, String idNum, String phone, String rank, String line,
 			String station, String level) {
 		
@@ -86,7 +101,12 @@ public class EmployeeManagement {
 	}
 	
 
-	
+	/**
+	 * 관리자정보가 들어있는 ArrayList에서 매개변수 word가 포함된 객체를 
+	 * ArrayList에 저장해서 리턴해주는 메서드
+	 * @param word 찾고싶은 검색어
+	 * @return 매개변수가 포함된 객체를 저장한 ArrayList
+	 */
 	public static ArrayList<EmployeeManagement> search(String word) {
 		
 		ArrayList<EmployeeManagement> list = new ArrayList<>();
@@ -107,7 +127,12 @@ public class EmployeeManagement {
 
 	}
 
-
+	/**
+	 * 매개변수 word를 검색해서 파일에 저장된 관리자 정보와 일치하는지 확인하는 메서드
+	 * @param e 관리자 객체
+	 * @param word 검색어
+	 * @return word 매개변수중 근무지(역), 이름, 호선, 직급중 하나라도 일치하면 true를 리턴
+	 */
 	private static boolean isExist(EmployeeManagement e, String word) {
 
 		if(e.getStation().equals(word)) {
@@ -132,7 +157,7 @@ public class EmployeeManagement {
 	@Override
 	public String toString() {
 		return String.format(
-				"[이름: %s, 아이디: %s, 전화번호: %s, 직급: %s, 호선: %s, 역: %s, 접근권한: %s]",
+				"[이름: %s, 아이디: %s, 전화번호: %s, 직급: %5s, 호선: %s, 역: %5s, 접근권한: %s]",
 				name, id, phone, rank, line, station, level);
 	}
 	
--- src/com/java/member/employee/Main.java
+++ src/com/java/member/employee/Main.java
@@ -9,6 +9,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.Scanner;
 
 import com.java.common.Data;
 import com.java.member.Member;
@@ -19,7 +20,7 @@ public class Main {
 		
 		
 		String root = "dat\\직원목록.txt";
-		
+		boolean loop = true;
 		Path path = Paths.get(root);
 		
 		List<String> line = Files.readAllLines(path);
@@ -37,171 +38,202 @@ public class Main {
 		//데이터 입력
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		
-		
-		
-		System.out.println("1.직원검색 2.직원수정 3.안전요원 관리");
-		String sel = reader.readLine();
+		while(loop) {
 
-		
-		
-		//직원 검색
-		if(sel.equals("1")) {
-			
-			System.out.println("1.호선으로 2.역이름으로 3.이름으로 4. 전체직원");
-			sel = reader.readLine();
-			
-			if(sel.equals("1")) { //호선으로
-				
-				System.out.print("호선: ");
-				String lines = reader.readLine();
-				
-				EmployeeInfo.searchEmployee(lines);
-				
-				
-			}else if(sel.equals("2")) { //역이름으로
-				
-				System.out.print("역 이름: ");
-				String station = reader.readLine();
-				EmployeeInfo.searchEmployee(station);
 
-				
-			}else if(sel.equals("3")) { //이름으로
-				
-				System.out.print("이름: ");
-				String name = reader.readLine();
-				EmployeeInfo.searchEmployee(name);
-				
-			}else if(sel.equals("4")) { //전체 직원
-				
-				EmployeeInfo.printAllEmployee();
-				
-			}
-			
-			
-			
-			
-		}else if(sel.equals("2")) { //직원 수정
-			
-			try {
-				
-				//관리자이면서 접근권한이 4이상인 관리자만 수정 가능
-				
-				if(Member.auth.equals("관리자") && (Member.level.equals("4") || Member.level.equals("5"))){
+
+			System.out.println("1.직원검색 2.직원수정 3.안전요원 관리 4.종료");
+			String sel = reader.readLine();
+
+
+
+			//직원 검색
+			if(sel.equals("1")) {
+
+				System.out.println("1.호선으로 2.역이름으로 3.이름으로 4. 전체직원");
+				sel = reader.readLine();
+
+				if(sel.equals("1")) { //호선으로
+
+					System.out.print("호선: ");
+					String lines = reader.readLine();
+
+					EmployeeInfo.searchEmployee(lines);
+
+
+				}else if(sel.equals("2")) { //역이름으로
+
+					System.out.print("역 이름: ");
+					String station = reader.readLine();
+					EmployeeInfo.searchEmployee(station);
+
+
+				}else if(sel.equals("3")) { //이름으로
+
+					System.out.print("이름: ");
+					String name = reader.readLine();
+					EmployeeInfo.searchEmployee(name);
+
+				}else if(sel.equals("4")) { //전체 직원
+
+					EmployeeInfo.printAllEmployee();
+
+				}
+
+
+
+
+			}else if(sel.equals("2")) { //직원 수정
+
+				try {
+
+					//관리자이면서 접근권한이 4이상인 관리자만 수정 가능
+
+					if(Member.auth.equals("관리자") && (Member.level.equals("4") || Member.level.equals("5"))){
+
+
+
+						System.out.print("수정 할 항목(근무지,직급,삭제):  ");
+						String word = reader.readLine();
+
+
+
+						// 수정할 항목은 나중에 숫자로 변경
+
+						if(word.equals("근무지")) {
+
+							System.out.print("수정할 직원 id : ");
+							String id = reader.readLine();
+							System.out.print("수정 할 호선 : ");
+							String lines = reader.readLine();
+
+							System.out.print("수정 할 근무지: ");
+							String station = reader.readLine();
+
+							EmployeeUpdate.changeStation(id, lines, station);
+
+
+						}else if(word.equals("직급")) {
+
+							System.out.print("수정할 직원 id : ");
+							String id = reader.readLine();
+
+							System.out.print("변경할 직급: ");
+							String rank = reader.readLine();
+
+
+							EmployeeUpdate.changeRank(id, rank);
+
+						}else if(word.equals("삭제")) {
+
+							System.out.print("삭제할 직원 id : ");
+							String id = reader.readLine();
+
+							EmployeeUpdate.removeEmployee(id);
+
+						}else if(word.equals("검색")) {
+
+						}
+
+
+					}
+				} catch (Exception e2) {
+					System.out.println("Main.main");
+					e2.printStackTrace();
+				}
+
+			}else if(sel.equals("3")) { //안전요원 관리
+
+				System.out.print("1. 안전요원 목록 2. 안전요원 배치 3. 안전요원 부서 해제");
+				sel = reader.readLine();
+
+				if(sel.equals("1")) { //안전 요원 목록
 					
+					System.out.print("1. 전체 안전요원 2. 호선으로 검색 3. 역 이름으로 검색");
+					sel = reader.readLine();
 					
-					
-					System.out.print("수정 할 항목(근무지,직급,삭제):  ");
-					String word = reader.readLine();
-					
-					
-					
-					// 수정할 항목은 나중에 숫자로 변경
-					
-					if(word.equals("근무지")) {
+					if(sel.equals("1")) { //전체 안전요원
 						
-						System.out.print("수정할 직원 id : ");
-						String id = reader.readLine();
-						System.out.print("수정 할 호선 : ");
-						String lines = reader.readLine();
+						EmployeeInfo.searchEmployee("안전요원");
 						
-						System.out.print("수정 할 근무지: ");
-						String station = reader.readLine();
+					}else if(sel.equals("2")) { //호선으로 검색
 						
-						EmployeeUpdate.changeStation(id, lines, station);
+						System.out.print("검색할 호선: ");
+						sel = reader.readLine();
 						
+						EmployeeInfo.searchSecurityEmployee(sel);
 						
-					}else if(word.equals("직급")) {
+					}else if(sel.equals("3")) { //역이름으로 검색
 						
-						System.out.print("수정할 직원 id : ");
-						String id = reader.readLine();
+						System.out.print("검색할 역 이름: ");
+						sel = reader.readLine();
 						
-						System.out.print("변경할 직급: ");
-						String rank = reader.readLine();
-						
-						
-						EmployeeUpdate.changeRank(id, rank);
-						
-					}else if(word.equals("삭제")) {
-						
-						System.out.print("삭제할 직원 id : ");
-						String id = reader.readLine();
-						
-						EmployeeUpdate.removeEmployee(id);
-						
-					}else if(word.equals("검색")) {
+						EmployeeInfo.searchSecurityEmployee(sel);
 						
 					}
-					
-					
+
+
+
+				}else if(sel.equals("2")) { //안전요원 배치
+
+
+					EmployeeInfo.searchEmployee("안전요원");
+
+					Scanner scan = new Scanner(System.in);
+
+					System.out.print("id 입력: ");
+					String id = scan.nextLine();
+
+					System.out.print("호선 입력: ");
+					String lines = scan.nextLine();
+
+					System.out.print("역 입력: ");
+					String station = scan.nextLine();
+
+
+					EmployeeUpdate.changeStation(id, lines, station);
+
+
+				}else if(sel.equals("3")) { //안전요원 부서 해제
+
+					EmployeeInfo.searchEmployee("안전요원");
+
+					System.out.print("id 입력: ");
+					String id = reader.readLine();
+
+
+
+					EmployeeUpdate.changeStation(id, "미배치", "미배치");
+
+
+
+
 				}
-			} catch (Exception e2) {
-				System.out.println("Main.main");
-				e2.printStackTrace();
+
+
+
+
+			}else if(sel.equals("4")) {
+				System.out.println("프로그램을 종료합니다.");
+				loop=false;
 			}
-			
-		}else if(sel.equals("3")) { //안전요원 관리
-			
-			System.out.print("1. 전체 안전요원보기 2. 안전요원 배치 3. 안전요원 부서 해제");
-			sel = reader.readLine();
-			
-			if(sel.equals("1")) { //전체 안전요원 보기
-				
-				EmployeeInfo.searchEmployee("안전요원");
-				
-				
-			}else if(sel.equals("2")) { //안전요원 배치
-				
-				EmployeeInfo.searchEmployee("안전요원");
-				
-				System.out.print("id 입력: ");
-				String id = reader.readLine();
-				
-				System.out.print("호선 입력: ");
-				String lines = reader.readLine();
-				
-				System.out.print("역 입력: ");
-				String station = reader.readLine();
-						
-				
-				EmployeeUpdate.changeStation(id, lines, station);
-				
-				
-			}else if(sel.equals("3")) { //안전요원 부서 해제
-				
-				EmployeeInfo.searchEmployee("안전요원");
-				
-				System.out.print("id 입력: ");
-				String id = reader.readLine();
-				
-						
-				
-				EmployeeUpdate.changeStation(id, "부서해제", "부서해제");
-				
-				
-				
-				
-			}
-			
-			
-			
-			
 		}
-		
-		
-			BufferedWriter writer = new BufferedWriter(new FileWriter(root));
-			
-			
-			//파일 저장 csv파일 형식
-			for(EmployeeManagement e : Data.employeeList) {
-				
-				String str = String.format("%s,%s,%s,%s,%s,%s,%s,%s,%s", e.getId(),e.getPw(),e.getName(),e.getIdNum(),e.getPhone(),e.getRank(),e.getLine(),e.getStation(),e.getLevel());
-				writer.write(str);
-				writer.newLine();
-				
-				
-			}
-			
-			writer.close();
+
+
+		BufferedWriter writer = new BufferedWriter(new FileWriter(root));
+
+
+		//파일 저장 csv파일 형식
+		for(EmployeeManagement e : Data.employeeList) {
+
+			String str = String.format("%s,%s,%s,%s,%s,%s,%s,%s,%s", e.getId(),e.getPw(),e.getName(),e.getIdNum(),e.getPhone(),e.getRank(),e.getLine(),e.getStation(),e.getLevel());
+			writer.write(str);
+			writer.newLine();
+
+
+		}
+
+		writer.close();
 			
 		
 		

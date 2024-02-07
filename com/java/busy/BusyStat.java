c4a83c8aac4553d533ec65c5809af72ef30308b0
Kijun0708 <ajud71@naver.com>
2024-02-07 15:44:32 +0900

    update

--- com/java/busy/BusyStat.java
+++ com/java/busy/BusyStat.java
@@ -3,6 +3,7 @@ package com.java.busy;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import com.java.common.Validation;
 import com.java.common.log.LogSave;
@@ -107,19 +108,65 @@ public class BusyStat extends StationManagement{
 	//혼잡도 인덱스 총 15개
 	private void printBusy(ArrayList<Busy> list, String line, String way, String dayOfWeek, String time) {
 		
-		String result = String.format("호선: %s, 방향: %s, 요일: %s\n",line,way,dayOfWeek);
+		ArrayList<String> busyStatPage = new ArrayList<>();
 		
-		System.out.println(result);
+		
+		String title = String.format("\t\t\t호선: %s, 방향: %s, 요일: %s\n",line,way,dayOfWeek);
+		String page = "";
 		
 		for(Busy b : list) {
 			
-			System.out.printf("%-35s  \t: %-3.1f",b.getStation()+"역",b.getCrowded().get(Integer.parseInt(time)-5));
-			System.out.println();
-			
+			page = String.format("\t%-35s  \t: %-3.1f\n",b.getStation()+"역",b.getCrowded().get(Integer.parseInt(time)-5));
+			busyStatPage.add(page);
 		}
 		
 		LogSave.logSave(LogSave.BUSYSTAT);
-		
+		busyStatPage(busyStatPage, title);
+	}
+	
+	public static void busyStatPage(ArrayList<String> list, String title) {
+		// 리스트의 페이지수 계산
+		int page = (int)(Math.ceil((double)list.size() / 5));
+
+		int index = 0;		// 문자로 입력받은 숫자를 int로 변환
+
+		Scanner scan = new Scanner(System.in);
+
+		while(true) {
+
+			String sel = "";	// 입력받는 문자열
+
+			// View클래스 출력
+			ViewAll.employeeSearch();
+
+			System.out.println(title);
+			list.stream().skip(index * 5)
+			.limit(5)
+			.forEach(busy -> System.out.println(busy));
+			// 이름, ID, 전화번호, 직급, 호선, 역이름
+			System.out.printf("Page| %s / %s\r\n", index + 1, page);
+			System.out.println("엔터입력시 리스트보기를 종료합니다.");
+			System.out.print("원하는 페이지: ");
+			sel = scan.nextLine();
+
+			if(sel.equals("")) {
+				break;
+			}else if (Validation.is_NumString(sel)) {
+				index = Integer.parseInt(sel) - 1;
+
+				if(index < 0 || index >= page) {
+					System.out.println("페이지 범위를 벗어났습니다.");
+					System.out.println("다시 입력해주세요.");
+					index = 0;
+
+				}
+
+			}else {
+				System.out.println("잘못된 입력입니다.");
+				System.out.println("다시 입력해주세요.");
+			}
+
+		}//while루프 종료
 	}
 	
 	

881aac3097d82868120ea950bb49b91a28d03ddf
Kijun0708 <ajud71@naver.com>
2024-02-13 23:40:00 +0900

    [UPDATE] lostarticle 패키지 주석

--- com/java/common/lostarticle/LostArticle.java
+++ com/java/common/lostarticle/LostArticle.java
@@ -1,14 +1,36 @@
 package com.java.common.lostarticle;
 
 // 분실물 클래스
+/**
+ * 분실물 정보를 저장하는 클래스
+ */
 public final class LostArticle {
 	
+	/**
+	 * 분실물 이름을 저장하는 멤버 변수
+	 */
 	private String article;
+	/**
+	 * 분실물 내용을 저장하는 멤버 변수
+	 */
 	private String content;
+	/**
+	 * 분실물 잃어버린 역 이름을 저장하는 변수
+	 */
 	private String lostStation;
+	/**
+	 * 분실물 보관중인 역 이름을 저장하는 변수
+	 */
 	private String findStation;
 	
 	// 내용이 있는 분실물 객체 생성자
+	/**
+	 * 내용이 있는 분실물 객체 생성자
+	 * @param article 분실물 이름
+	 * @param content 내용
+	 * @param lostStation 잃어버린 역 이름
+	 * @param findStation 보관중인 역 이름
+	 */
 	public LostArticle(String article, String content, String lostStation, String findStation) {
 		this.article = article;
 		this.content = content;
@@ -17,6 +39,12 @@ public final class LostArticle {
 	}
 	
 	// 내용이 없는 분실물 객체 생성자
+	/**
+	 * 내용이 없는 분실물 객체 생성자
+	 * @param article 분실물 이름
+	 * @param lostStation 잃어버린 역 이름
+	 * @param findStation 보관중인 역 이름
+	 */
 	public LostArticle(String article, String lostStation, String findStation) {
 		this.article = article;
 		this.content = "";
@@ -24,15 +52,31 @@ public final class LostArticle {
 		this.findStation = findStation;
 	}
 	
+	/**
+	 * 분실물 이름을 리턴하는 메서드
+	 * @return 분실물 이름
+	 */
 	public String getArticle() {
 		return article;
 	}
+	/**
+	 * 분실물 내용을 리턴하는 메서드
+	 * @return 분실물 내용
+	 */
 	public String getContent() {
 		return content;
 	}
+	/**
+	 * 분실물 잃어버린 역 이름을 리턴하는 메서드
+	 * @return 잃어버린 역 이름
+	 */
 	public String getLostStation() {
 		return lostStation;
 	}
+	/**
+	 * 분실물 보관중인 역 이름을 리턴하는 메서드
+	 * @return 보관중인 역 이름
+	 */
 	public String getFindStation() {
 		return findStation;
 	}
--- com/java/common/lostarticle/LostArticleTab.java
+++ com/java/common/lostarticle/LostArticleTab.java
@@ -8,14 +8,22 @@ import com.java.common.Validation;
 import com.java.common.log.LogSave;
 import com.java.view.ViewAll;
 
+/**
+ * 분실물 메뉴를 나타내는 클래스
+ */
 public class LostArticleTab {
 	
-	
+	/**
+	 * 분실물 메뉴를 나타내는 메서드
+	 */
 	public static void lostArticleTab() {
 		
 		Scanner scan = new Scanner(System.in);
 		
 		while(true) {
+			/**
+			 * 입력받은 값을 저장하는 변수
+			 */
 			String sel = ""; // 선택한 번호
 			
 			// 모든 분실물 먼저 보여주기
@@ -47,6 +55,9 @@ public class LostArticleTab {
 		
 	}//End of lostArticleTab()
 	
+	/**
+	 * 분실물을 검색하는 메서드
+	 */
 	public static void lostArticleSearch() {
 			
 			Scanner scan = new Scanner(System.in);
@@ -54,10 +65,16 @@ public class LostArticleTab {
 			ViewAll.lostarticleSearchTop();
 			System.out.println();
 			
+			/**
+			 * 분실물 정보를 저장하는 ArrayList
+			 */
 			ArrayList<LostArticle> list = new ArrayList<LostArticle>();
 			
 			
 			while(true) {
+				/**
+				 * 입력한 값을 저장하는 변수
+				 */
 				String search ="";
 				
 				System.out.println("\t\t\t찾으실 분실물을 검색해주세요.");
@@ -68,9 +85,15 @@ public class LostArticleTab {
 					System.out.println("\t\t\t잘못된 입력입니다.");
 					break;
 				}
-					
+				
+				/**
+				 *  입력한 값을 저장하는 변수
+				 */
 				final String SEARCH = search;
 				
+				/**
+				 * 일치 여부를 저장하는 변수
+				 */
 				boolean check = Data.lostArticleList.stream().anyMatch(article -> article.getArticle().contains(SEARCH));
 				
 				if(check) {		// 1개라도 있으면
@@ -103,6 +126,9 @@ public class LostArticleTab {
 		
 	}//End of lostArticle()
 	
+	/**
+	 * 분실물을 추가하는 메서드
+	 */
 	public static void addLostArticle() {
 		
 		Scanner scan = new Scanner(System.in);
@@ -110,10 +136,25 @@ public class LostArticleTab {
 		
 		while(true) {
 			
+			/**
+			 * 입력받은 값을 저장하는 변수
+			 */
 			String sel = "";
+			/**
+			 * 분실물 이름을 저장하는 변수
+			 */
 			String article = "";
+			/**
+			 * 분실물 내용을 저장하는 변수
+			 */
 			String content = "";
+			/**
+			 * 분실물 찾은 위치를 저장하는 변수
+			 */
 			String find = "";
+			/**
+			 * 분실물 보관 위치를 저장하는 변수
+			 */
 			String keep = "";
 			
 			ViewAll.lostarticleAdd();
@@ -187,6 +228,9 @@ public class LostArticleTab {
 		
 	}//End of addLostArticle()
 	
+	/**
+	 * 분실물을 제거하는 변수
+	 */
 	public static void removeLostArticle() {
 		
 		Scanner scan = new Scanner(System.in);
@@ -195,7 +239,13 @@ public class LostArticleTab {
 		while(true) {
 			
 			LostArticleSearch.lostArticlePage(Data.lostArticleList);
+			/**
+			 * 입력한 값인 분실물의 이름을 저장하는 변수
+			 */
 			String sel = "";
+			/**
+			 * 입력한 값인 보관 된 위치를 저장하는 변수
+			 */
 			String sts = "";
 			
 			System.out.println("\t\t\t삭제할 분실물의 이름");
@@ -248,7 +298,9 @@ public class LostArticleTab {
 		}//while루프 종료
 		
 	}//End of removeLostArticle()
-	
+	/**
+	 * 모든 분실물을 보는 메서드
+	 */
 	public static void lostArticleAll() {
 		
 		LostArticleSearch.lostArticlePage(Data.lostArticleList);
